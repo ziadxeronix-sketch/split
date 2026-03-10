d39080c8d9c11126a8cbb0680907334576b39988
Ziad Mohamed <zimo1590@gmail.com>
2026-03-10 16:20:55 +0200

    Add files via upload

---
+++ ios/Runner/AppDelegate.swift
@@ -0,0 +1,18 @@
+import Flutter
+import UIKit
+import FirebaseCore
+
+@main
+@objc class AppDelegate: FlutterAppDelegate {
+  override func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    if FirebaseApp.app() == nil {
+      FirebaseApp.configure()
+    }
+
+    GeneratedPluginRegistrant.register(with: self)
+    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+}
